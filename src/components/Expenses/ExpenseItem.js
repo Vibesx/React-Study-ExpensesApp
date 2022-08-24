295f06f7b15469779077c0897c197eb74dc7c1ee
vibesx <lucian.paslariu@gmail.com>
2022-08-24 11:56:44 +0300

    added basic usestate

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 import "./ExpenseItem.css";
 import ExpenseDate from "./ExpenseDate";
 import Card from "../UI/Card";
@@ -6,14 +8,25 @@ import Card from "../UI/Card";
 // and props in this case will include a property named test with the value of "asd", callable through props.test
 // NOTE: the param here can have any name, but we use props for clarity
 export default function ExpenseItem(props) {
+	// the useState function returns an array; first element is the managed value and the second is a function that allows us to modifiy the value
+	// the parameter of useState is the default value we want
+	const [title, setTitle] = useState(props.title);
+
+	// a good practice is to name functions used by events with <action>Handler
+	const clickHandler = () => {
+		// the setter function provided by useState is a special function that changes our value AND also triggers a rerender of the component (recalls the ExpenseItem function)
+		setTitle("TestTitle");
+		console.log(title);
+	};
+
 	return (
 		<Card className="expense-item">
 			<ExpenseDate date={props.date}></ExpenseDate>
 			<div className="expense-item__description">
-				<h2>{props.title}</h2>
+				<h2>{title}</h2>
 				<div className="expense-item__price">${props.amount}</div>
 			</div>
-			<button>Change Title</button>
+			<button onClick={clickHandler}>Change Title</button>
 		</Card>
 	);
 }
